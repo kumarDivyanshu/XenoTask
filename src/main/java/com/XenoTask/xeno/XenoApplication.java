974497fca7c264d1e61479835fdbb5cf15359818
Divyanshu Kumar <divyanshukumar258@gmail.com>
2025-09-12 00:01:43 +0530

    start of project

---
+++ src/main/java/com/XenoTask/xeno/XenoApplication.java
@@ -0,0 +1,13 @@
+package com.XenoTask.xeno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XenoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XenoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/XenoTask/xeno/XenoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.XenoTask.xeno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
